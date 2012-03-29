9a1787ebd7240d7bfe196263375634d04028c7a4
Andrew Watson <andrewmoorewatson@gmail.com>
2012-03-29 19:38:21 -0400

    back to ini

--- test.php
+++ test.php
@@ -2,30 +2,23 @@
 
 	try {
 
-	$config = array(
-			'S3' => array(
-				'key' => getenv('AWS_ACCESS_KEY_ID'), 
-				'secret' => getenv('AWS_SECRET_KEY'),
-				'bucket' => 'mixee-static-assets'
+		$config = parse_ini_file("config.ini", true);
+		include("sdk-1.5.3/sdk.class.php");
+
+		$s3 = new AmazonS3( 
+			array(
+				'key' => $config['S3']['key'],
+				'secret' => $config['S3']['secret']
 			)
-	);
-	var_dump($config);
-	die;
+		);
 
+		if (!$s3->if_bucket_exists( $config['S3']['bucket'])) {
+			throw new Exception("BAD BUCKET {$config['S3']['bucket']}");
+		}
 
-	include("sdk-1.5.3/sdk.class.php");
-
-	$s3 = new AmazonS3( 
-		array(
-			'key' => $config['S3']['key'],
-			'secret' => $config['S3']['secret']
-		)
-	);
-
-	if (!$s3->if_bucket_exists( $config['S3']['bucket'])) {
-		throw new Exception("BAD BUCKET {$config['S3']['bucket']}");
-	}
 	} catch (Exception $e) {
 		echo $e->getMessage();
 	}
+
+	echo "let's go...";
 ?>
