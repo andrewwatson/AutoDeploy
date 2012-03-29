7af20116fa414767731bc927c5678fc61dea7367
Andrew Watson <andrewmoorewatson@gmail.com>
2012-03-29 19:43:10 -0400

    bucket as get

--- test.php
+++ test.php
@@ -5,6 +5,8 @@
 		$config = parse_ini_file("config.ini", true);
 		include("sdk-1.5.3/sdk.class.php");
 
+		$bucket = $_GET['bucket'];
+
 		$s3 = new AmazonS3( 
 			array(
 				'key' => $config['S3']['key'],
@@ -12,13 +14,14 @@
 			)
 		);
 
-		if (!$s3->if_bucket_exists( $config['S3']['bucket'])) {
-			throw new Exception("BAD BUCKET {$config['S3']['bucket']}");
+		if (!$s3->if_bucket_exists( $bucket)) {
+			throw new Exception("BAD BUCKET {$bucket)");
 		}
+
+
+		echo "let's go...";
 
 	} catch (Exception $e) {
 		echo $e->getMessage();
 	}
-
-	echo "let's go...";
 ?>
