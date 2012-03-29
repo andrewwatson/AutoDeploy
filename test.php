4301461c155f6ad78f09fa8781b8d22e40f3e6dc
Andrew Watson <andrewmoorewatson@gmail.com>
2012-03-29 00:58:34 -0400

    test

--- test.php
+++ test.php
@@ -1,4 +1,26 @@
 <?php
 
+	try {
+
+	$config = array(
+			'S3' => array(
+				'key' => getenv('AWS_ACCESS_KEY_ID'), 
+				'secret' => getenv('AWS_SECRET_KEY'),
+				'bucket' => getenv('PARAM1')
+			)
+	);
+
+	include("sdk-1.5.3/sdk.class.php");
+
+	$s3 = new AmazonS3( 
+		array(
+			'key' => $config['S3']['key'],
+			'secret' => $config['S3']['secret']
+		)
+	);
+
+	} catch (Exception $e) {
+		echo $e->getMessage();
+	}
 ?>
 FOO
