4ad0756c74e400933f1e7bcf7e440fc228b7a21a
Andrew Watson <andrewmoorewatson@gmail.com>
2012-04-07 17:02:35 -0400

    updated relay script

--- relay.php
+++ relay.php
@@ -12,6 +12,12 @@
 
 		include("AWSSDKforPHP/sdk.class.php");
 
+		$sqs = new AmazonSQS(
+			array(
+				'key' => $config['S3']['key'],
+				'secret' => $config['S3']['secret']
+			)
+		);
 
 		$s3 = new AmazonS3( 
 			array(
@@ -24,6 +30,34 @@
 			throw new Exception("BAD BUCKET {$bucket}");
 		}
 
+		$resp = $sqs->receive_message($config['SQS']['queue']);
+
+		if (!empty($resp->body->ReceiveMessageResult->Message)) {
+			$handle = $resp->body->ReceiveMessageResult->Message->ReceiptHandle;
+			$body = $resp->body->ReceiveMessageResult->Message->Body;
+			$message = json_decode($body);
+
+			chdir($message->repo);
+			system("git pull origin master");
+
+			foreach ($message->changed_files as $filename) {
+				echo "Uploading {$filename}\n";
+         	$s3->batch()->create_object($bucket, $filename, array( 'fileUpload' => $filename));
+
+			}
+
+			$s3->batch()->send();
+			
+			// remove the message from the queue
+			$d_resp = $sqs->delete_message($config['SQS']['queue'], $handle);
+			if (!$d_resp->isOK()) {
+				var_dump($d_resp);
+			}
+		}
+
+
 	} catch (Exception $e) {
+		error_log($e->getMessage());
+		echo $e->getMessage();
 
 	}
